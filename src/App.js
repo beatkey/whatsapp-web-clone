5f520bc2b475347f96b226ecef046a2fdb0d267d
Beatkey <emrecaal@gmail.com>
2022-09-28 19:31:31 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 function App() {
-  return (
-    <div className="App">
-      a
-    </div>
-  );
+    return (
+        <div className="App px-10 py-5">
+            <div className="LeftSide bg-color2 w-[30%] h-[60px]">
+                <div className="Head">
+                    <div className="Profile">
+
+                    </div>
+                    <div className="Actions">
+                        <div className="Story">
+
+                        </div>
+                        <div className="Message">
+
+                        </div>
+                        <div className="Settings">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="Search">
+
+                </div>
+                <div className="Messages">
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        colors: {
+            'color1': '#111b21',
+            'color2': '#1f2c33'
+        },
+        fontFamily: {
+            sans: ['Segoe UI,Helvetica Neue,Helvetica,Lucida Grande,Arial,Ubuntu,Cantarell,Fira Sans', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
 }
