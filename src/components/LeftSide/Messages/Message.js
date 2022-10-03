43a1b1d34f2961d525051552454bb42fe16e2c6d
Emre Cal <emrecaal@gmail.com>
2022-10-04 00:39:45 +0300

    message dropdown menu

--- src/components/LeftSide/Messages/Message.js
+++ src/components/LeftSide/Messages/Message.js
@@ -1,4 +1,19 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 export default function Message({value, activeMessageHandle, index, activeMessage}) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        event.preventDefault()
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     const Avatar = ({type}) => {
         if (type === "svg-group") {
             return (
@@ -69,7 +84,8 @@ export default function Message({value, activeMessageHandle, index, activeMessag
             <div className="Img mr-3">
                 <Avatar type={value.image}/>
             </div>
-            <div className="w-full border-t border-[rgba(134,150,160,0.15)] group-hover:border-[transparent] py-3 pr-3.5">
+            <div
+                className="w-full border-t border-[rgba(134,150,160,0.15)] group-hover:border-[transparent] py-3 pr-3.5">
                 <div className="Top flex items-center justify-between text-[#e9edef] text-[17px]">
                     <div>
                         {value.name}
@@ -83,12 +99,32 @@ export default function Message({value, activeMessageHandle, index, activeMessag
                         <Status type={value.status}/>
                         {value.message}
                     </div>
-                    <div className="absolute right-[-50px] group-hover:right-0">
+                    <div id="basic-button"
+                         aria-controls={open ? 'basic-menu' : undefined}
+                         aria-haspopup="true"
+                         aria-expanded={open ? 'true' : undefined}
+                         onClick={handleClick}
+                         className={"absolute right-[-50px] group-hover:right-0 transition-all duration-150 " + (open ? "right-0" : "")}>
                         <svg viewBox="0 0 19 20" width="19" height="20" className="">
                             <path fill="currentColor"
                                   d="m3.8 6.7 5.7 5.7 5.7-5.7 1.6 1.6-7.3 7.2-7.3-7.2 1.6-1.6z"></path>
                         </svg>
                     </div>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>Archive Chat</MenuItem>
+                        <MenuItem onClick={handleClose}>Mute notifications</MenuItem>
+                        <MenuItem onClick={handleClose}>Mark As Unread</MenuItem>
+                        <MenuItem onClick={handleClose}>Delete Chat</MenuItem>
+                        <MenuItem onClick={handleClose}>Pin Chat</MenuItem>
+                    </Menu>
                 </div>
             </div>
         </div>
